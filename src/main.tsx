140c98a0df3a93b56e5080123631bb7a2cf63d5a
Rhine <yuruidan528@gmail.com>
2026-04-29 16:37:34 +0800

    chore: use HashRouter for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { FeedProvider } from './context/FeedContext.tsx'
 import './index.css'
@@ -8,12 +8,12 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <FeedProvider>
           <App />
         </FeedProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
